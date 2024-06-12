8ade9344de1311a6342e12179d2d7086978bc111
김태건 <121171046+taegun-kim@users.noreply.github.com>
2024-06-12 23:18:09 +0900

    Update lcd_speed.ino

--- lcd_speed.ino
+++ lcd_speed.ino
@@ -8,70 +8,121 @@
 #endif
 
 const int Sigpin1 = 8; 
-const int Sigpin2 = 12;
+const int Sigpin2 = 12; 
+const int speed = 9;
+const int In1 = 5;
+const int In2 = 6;
+const int overSpeed = 5;
+const int Address = 0x50;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // 16x2 LCD의 주소를 0x27로 설정
-
+bool isOverspeed = false;
 bool isEmergency = false;
 
 int v1 = 0;
 int v2 = 0;
+int buffer = 0;
+
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // 16x2 LCD의 주소를 0x27로 설정
 
 void setup() {
+  u8g2.begin();
+  u8g2.enableUTF8Print();
   Serial.begin(115200);  // 시리얼 통신을 115200 보드레이트로 초기화
+  pinMode(Sigpin1, INPUT);
+  pinMode(Sigpin2, INPUT);
+  pinMode(speed, OUTPUT);
+  pinMode(In1, OUTPUT);
+  pinMode(In2, OUTPUT);
+  MsTimer2::set(500,playLcd);
+  MsTimer2::start();
+  actuatorUp();
   lcd.init();            // LCD 초기화
   lcd.backlight();       // 백라이트 켜기
   lcd.clear();           // LCD 화면 지우기
-  pinMode(Sigpin1, INPUT);
-  pinMode(Sigpin2, INPUT);
+}
+
+void drawMessage(const char* line1, const char* line2, int cursorX) {
+  u8g2.setFont(u8g2_font_unifont_t_korean1);
+  u8g2.setFontDirection(0);
+  u8g2.setCursor(20, 20);
+  u8g2.print(line1);
+  u8g2.setCursor(cursorX, 40);
+  u8g2.print(line2);
+}
+
+void playLcd() {
+u8g2.firstPage();
+  do {
+    if (analogRead(A4) < 700) {
+      drawMessage("비가 내리니", "안전 운행하세요", 5);
+    } else {
+      drawMessage("즐거운 하루", "되세요!", 40);
+    }
+  } while (u8g2.nextPage());
 }
 
 void speedCheck1() {
   while (digitalRead(Sigpin1));
   while (!digitalRead(Sigpin1));
-  unsigned long T = pulseIn(Sigpin1, HIGH, 1000000) + pulseIn(Sigpin1, LOW, 1000000); // 타임아웃 1초 설정
+  unsigned long T = pulseIn(Sigpin1, HIGH) + pulseIn(Sigpin1, LOW); // 0.1s안에 HIGH값 안들어오면 0처리
 
   if (T != 0)
   {
     double frequency = 1.0 / (double)T;
     v1 = int((frequency * 1e6) / 44.0);
 
-    if (v1 > 120) {
+    if (v1 > 120)
+    {
       v1 = 0;
     }
   } 
-  else { // 측정이 안된경우 
-    v1 = 0;
+  else
+  { // 측정이 안된경우 
+  v1 = 0;
   }
-  lcd.setCursor(0, 1);
-  lcd.print(v1);
 }
 
 void speedCheck2() {
   while (digitalRead(Sigpin2));
   while (!digitalRead(Sigpin2));
-  unsigned long T = pulseIn(Sigpin2, HIGH, 1000000) + pulseIn(Sigpin2, LOW, 1000000); // 타임아웃 1초 설정
+  unsigned long T = pulseIn(Sigpin2, HIGH) + pulseIn(Sigpin2, LOW); // 0.1s안에 HIGH값 안들어오면 0처리
 
   if (T != 0)
   {
     double frequency = 1.0 / (double)T;
     v2 = int((frequency * 1e6) / 44.0);
 
-    if (v2 > 120) {
+    if (v2 > 120)
+    {
       v2 = 0;
     }
   } 
-  else { // 측정이 안된경우 
-    v2 = 0;
+  else
+  { // 측정이 안된경우 
+  v2 = 0;
   }
-  lcd.setCursor(5, 1);
-  lcd.print(v2);
+}
+ 
+
+void actuatorDown() {
+  digitalWrite(In1, HIGH);
+  digitalWrite(In2, LOW);
+  analogWrite(speed, 255);
+}
+
+void actuatorUp() {
+  digitalWrite(In1, LOW);
+  digitalWrite(In2, HIGH);
+  analogWrite(speed, 255);
 }
 
 void detectEmergency() {
+  buffer = Serial.available();  // 시리얼 버퍼에 있는 바이트 수를 읽음
+  
   if (Serial.available() > 0) {
     char received = Serial.read();
-    isEmergency = (received == '1');
+    isEmergency = (received == '1') ? true : false; // 수정
 
     lcd.clear();  // LCD 화면 지우기
     lcd.setCursor(0, 0);  // 커서를 첫 번째 줄 첫 번째 열로 설정
@@ -82,26 +133,29 @@ void detectEmergency() {
       lcd.print("ambulance");  // 비상 상황이면 "ambulance" 출력
     }
 
-    lcd.setCursor(11, 0);  // 커서를 첫 번째 줄 11번째 열로 설정
-    lcd.print((int)Serial.available());  // 시리얼 버퍼에 있는 바이트 수 출력
+    lcd.setCursor(11, 0);  // 커서를 두 번째 줄 첫 번째 열로 설정
+    lcd.print(buffer);  // 시리얼 버퍼에 있는 바이트 수 출력
+    lcd.setCursor(0, 1);
+    lcd.print(v1);
+    lcd.setCursor(5, 1);
+    lcd.print(v2);
   }
 }
 
 void loop() {
-  static unsigned long lastCheck1 = 0;
-  static unsigned long lastCheck2 = 0;
-  unsigned long currentMillis = millis();
 
-  if (currentMillis - lastCheck1 >= 1000) { 
-    lastCheck1 = currentMillis;
-    speedCheck1();
-  }
-
-  // speedCheck2을 1초마다 실행
-  if (currentMillis - lastCheck2 >= 1000) { 
-    lastCheck2 = currentMillis;
-    speedCheck2();
-  }
+  speedCheck1();
+  speedCheck2();
 
   detectEmergency();
+  isOverspeed = (v1 >= overSpeed || v2 >= overSpeed) ? true : false; // 수정
+
+  if (isOverspeed){ 
+
+    if (!isEmergency){
+      actuatorDown();
+      delay(3000);
+      actuatorUp();
+    }
+  }
 }
